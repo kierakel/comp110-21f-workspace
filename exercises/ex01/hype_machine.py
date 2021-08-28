7cb950cf774892d587d7a03c056c2ff9531a364d
Kiera Kelly <kierakel@live.unc.edu>
2021-08-28 16:03:22 -0400

    progress on exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -3,6 +3,6 @@
 __author__ = "730350912"
 
 name: str = input("What is your name? ")
-print("You are going to kill your quiz, " + name +"!")
+print("You are going to kill your quiz, " + name + "!")
 print("I have all the faith in you, " + name + ".")
-print("Go get it, " + name + "!" )
+print("Go get it, " + name + "!")
