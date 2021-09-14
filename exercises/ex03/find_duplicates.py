d6667aa1de271fdc428d5d718bd52c1b1503e3c8
Kiera Kelly <kierakel@live.unc.edu>
2021-09-14 17:30:50 -0400

    exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,19 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730350912"
+
+word: str = input("Enter a word: ")
+duplicate: bool = False
+i: int = 0
+while i < len(word):
+    character = word[i]
+    j: int = i + 1
+    while j < len(word):
+        second = word[j]
+        j += 1
+        if character != second:
+            duplicate
+        else:
+            duplicate: bool = True
+    i += 1
+print("Found duplicate: " + str(duplicate))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,16 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730350912"
 
-# The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Depth: "))
+a = ""
+i: int = 0
+while i < depth:
+    if depth <= 0:
+        print("")
+    else: 
+        if i <= depth:
+            a = a + TREE
+            print(a)
+    i = i + 1
