fcf3901aaa7f2d7c82a2a3aa3d7f3e07f4f5423e
Kiera <kierakel@live.unc.edu>
2021-10-28 19:52:59 -0400

    commit

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -15,3 +15,21 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
         result.append(row)
     file_handle.close()
     return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """This function takes a table and returns a list of just the column values."""
+    empty: list[str] = []
+    for row in table:
+        key: str = row[column]
+        empty.append(key)
+    return empty
+
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """This function takes a row-oriented table and changes it to a column-oriented table."""
+    empty: dict[str, list[str]] = {}
+    row_1: dict[str, str] = table[0]
+    for column in row_1:
+        empty[column] = column_values(table, column)
+    return empty
