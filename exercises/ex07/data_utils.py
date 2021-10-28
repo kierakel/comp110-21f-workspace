6c88124ee535cdc207e00f189c2b349899e7b1ee
Kiera <kierakel@live.unc.edu>
2021-10-28 19:19:54 -0400

    commit

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -3,4 +3,15 @@
 __author__ = "730350912"
 
 
+from csv import DictReader
 
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """This function reads the CSV rows and puts them in a table."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
